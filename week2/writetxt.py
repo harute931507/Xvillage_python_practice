804cb8663c367dd563aa7d07244d998a695ef47c
harute0000 <harute0000@gmail.com>
2018-07-17 15:07:23 +0800

    week2

--- week2/writetxt.py
+++
@@ -1,9 +0,0 @@
-date = input('輸入日期:')
-event = input('輸入事件:')
-description = input('輸入心得:')
-
-with open("note.txt", "a+") as f:
-    f.writelines("日期 : %s\n" %date)
-    f.writelines("事件 : %s\n" %event)
-    f.writelines("心得 : %s\n\n" %description)
-    f.close()
